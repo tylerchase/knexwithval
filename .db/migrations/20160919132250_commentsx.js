1c2eecfbd85e7365ba817d936137dfc98155dd5a
Tyler Chase <tyler.chase.usa@gmail.com>
2016-09-19 13:25:14 -0600

    knex

---
+++ .db/migrations/20160919132250_commentsx.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('commentsx', function(table){
+    table.increments();
+    table.text('content');
+    table.integer('user_id').references('id').inTable('users').onDelete('CASCADE');
+    table.integer('post_id').references('id').inTable('posts').onDelete('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('commentsx')
+};
